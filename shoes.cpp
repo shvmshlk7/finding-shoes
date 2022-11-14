87602e97030e015cce87ddb59ecf7ad2cb329603
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2022-11-14 06:25:25 -0800

    Create shoes.cpp

---
+++ shoes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        
+        if(n<=m){
+            cout<<n<<endl;
+        }
+        else
+        {
+            cout<<(n-m)+n<<endl;
+        }
+    }
+	
+	return 0;
+}
